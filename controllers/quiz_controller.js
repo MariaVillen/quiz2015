a7e4944ef9903bc07ddb2420d1d1ada70f6db07c
MariaVillen <villenmj@gmail.com>
2015-06-23 15:04:50 +0200

    Despliegue en Heroku

---
+++ controllers/quiz_controller.js
@@ -0,0 +1,22 @@
+//Created in Chapter 6.5
+
+// GET /quizes/question
+
+
+
+// GET /quizes/answer
+
+
+exports.question = function(req, res){
+	res.render('quizes/question', {pregunta:'Capital de Italia'});
+
+};
+
+exports.answer = function(req, res){
+	if (req.query.respuesta === 'Roma'){
+		res.render('quizes/answer', {respuesta: 'Correcto'});
+	} else {
+		res.render('quizes/answer', {respuesta: 'Incorrecto'});
+	}
+
+};
