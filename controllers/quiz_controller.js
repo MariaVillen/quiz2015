24e37e576bfa842897d61fec82ce354c013aadc0
MariaVillen <villenmj@gmail.com>
2015-07-03 01:26:33 +0200

    Listado de Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,25 @@
 //Created in Chapter 6.5
 var models = require('../models/models.js'); //added chapter 7.2
 
+// GET / index
+
+exports.index = function( req, res){
+	models.Quiz.findAll().then(function(quizes) {
+		res.render('quizes/index.ejs', {quizes: quizes});
+	})
+};
+
 // GET /quizes/question
-exports.question = function(req, res){
-	models.Quiz.findAll().then(function(quiz){
-		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz: quiz});
 	})
 };
 
 // GET /quizes/answer
 exports.answer = function(req, res){
-	models.Quiz.findAll().then(function(quiz){
-		if (req.query.respuesta === quiz[0].respuesta){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		if (req.query.respuesta === quiz.respuesta){
 			res.render('quizes/answer', {respuesta: 'Correcto'});
 		} else {
 			res.render('quizes/answer', {respuesta: 'Incorrecto'});
--- routes/index.js
+++ routes/index.js
@@ -12,9 +12,11 @@ router.get('/', function(req, res) {
  	res.render('index', { title: 'Quiz' });
 });
 
-
-router.get('/quizes/question', quizController.question);//Added in Chapter 6.5
-router.get('/quizes/answer', quizController.answer);//Added in Chapter 6.5
+router.get('/quizes', quizController.index);//Added Chapter 7.4
+//router.get('/quizes/question', quizController.question);//Added in Chapter 6.5 
+//router.get('/quizes/answer', quizController.answer);//Added in Chapter 6.5
+router.get('/quizes/:quizID(\\d+)', quizController.show);//Added in Chapter 7.4 
+router.get('/quizes/:quizID(\\d+)/answer', quizController.answer);//Added in Chapter 7.4
 
 router.get('/author', function(req, res){
 	res.render('author');});
